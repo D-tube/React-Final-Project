f6905b2bde1963c4dca8d27288915f384c1ba15b
Ellison Matienzo <matienzo@sheridancollege.ca>
2022-03-29 18:30:16 -0400

    Bootstrap

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { Button } from 'react-bootstrap';
 
 const takePicture = () => {};
 const selectFile = () => {};
