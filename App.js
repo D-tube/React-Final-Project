ae30a98fb1db227642f8dba132694dd5ad0f10c4
Ellison Matienzo <matienzo@sheridancollege.ca>
2022-03-29 20:16:23 -0400

    Added Icons

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const takePicture = () => {};
 const selectFile = () => {};
@@ -11,10 +18,16 @@ const HelloWorldApp = () => {
       <View style={styles.space1} />
 
       <TouchableOpacity style={styles.button} onPress={takePicture}>
-        <Text style={styles.buttonText}>Take Picture</Text>
+        <View style={styles.row}>
+          <Text style={styles.buttonText}>Take Picture</Text>
+          <Image source={require('./img/camera.png')} style={styles.cameraIcon} />
+        </View>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={selectFile}>
-        <Text style={styles.buttonText}>Select File</Text>
+        <View style={styles.row}>
+          <Text style={styles.buttonText}>Select File</Text>
+          <Image source={require('./img/document.png')} style={styles.docIcon} />
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -35,13 +48,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#51BAC9',
     marginTop: 10,
     marginBottom: 10,
-    height: 50,
+    height: 100,
   },
   buttonText: {
+    marginLeft: 30,
+    marginTop: 26,
     color: '#ffffff',
     fontSize: 30,
     textAlign: 'center',
     alignItems: 'center',
+  },
+  cameraIcon: {
+    marginRight: 30,
+    marginTop: 30,
+    width: 65,
+    height: 40,
+  },
+  docIcon: {
+    marginRight: 40,
+    marginTop: 24,
+    width: 40,
+    height: 50,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   space1: {
     width: 1,
